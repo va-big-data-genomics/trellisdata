769f2e599f2527e0a4229bfd6c17f1515c84b790
Paul Billing-Ross <pbilling@stanford.edu>
2022-03-24 17:21:45 -0700

    Add TaxonomyParser

--- src/trellisdata/utils.py
+++ src/trellisdata/utils.py
@@ -1,3 +1,126 @@
+import pytz
+
+from datetime import datetime
+
+class TaxonomyParser:
+    """
+    This class is a wrapper on a Tree class from the anytree library to hold
+    different data hierarchies read from a JSON representation
+
+    Source: https://towardsdatascience.com/represent-hierarchical-data-in-python-cd36ada5c71a
+    """
+
+    def __init__(self, level_prefix = "L"):
+        self.prefix = level_prefix
+        self.nodes = {}
+        self.root_key = None
+
+    def find_by_name(self, name) -> Node:
+        """
+        Retrieve a node by its unique identifier name
+        """
+        root = self.nodes[self.root_key]
+        node = find(root, lambda node: node.name == name)
+        return node
+   
+    def read_from_json(self, fname):
+        """
+        Read the taxonomy from a JSON file given as input
+        """
+        
+        self.nodes = {}
+        try:
+            with open(fname, "r") as f:
+                data = json.load(f)
+                n_levels = len(list(data.keys()))
+
+                # read the root node
+                root = data[f"{self.prefix}0"][0]
+                name = root["name"]
+                _ = root.pop("name")
+                
+                self.nodes[name] = Node(name, **root)
+                self.root_key = name
+
+                # populate the tree
+                for k in range(1, n_levels):
+                    
+                    key = f"{self.prefix}{k}"
+                    nodes = data[key]
+
+                    for n in nodes:
+                        try:
+                            assert "name" in n
+                            name = n["name"]
+                            _ = n.pop("name")
+                            parent = n["parent"]
+                            _ = n.pop("parent")
+                            
+                            self.nodes[name] = Node(
+                                name,
+                                parent=self.nodes[parent],
+                                **n
+                            )
+                        except AssertionError:
+                            print(f"Malformed node representation: {n}")
+                        except KeyError:
+                            print(f"Detected a dangling node: {n['name']}")
+
+        except (FileNotFoundError, KeyError):
+            raise Exception("Not existent or malformed input JSON file")
+
+    def read_from_string(self, data_str):
+        """
+        Read the taxonomy from a JSON file given as input
+        """
+        
+        self.nodes = {}
+        try:
+            data = json.loads(data_str)
+            n_levels = len(list(data.keys()))
+
+            # read the root node
+            root = data[f"{self.prefix}0"][0]
+            name = root["name"]
+            _ = root.pop("name")
+            
+            self.nodes[name] = Node(name, **root)
+            self.root_key = name
+
+            # populate the tree
+            for k in range(1, n_levels):
+                
+                key = f"{self.prefix}{k}"
+                nodes = data[key]
+
+                for n in nodes:
+                    try:
+                        assert "name" in n
+                        name = n["name"]
+                        _ = n.pop("name")
+                        parent = n["parent"]
+                        _ = n.pop("parent")
+                        
+                        self.nodes[name] = Node(
+                            name,
+                            parent=self.nodes[parent],
+                            **n
+                        )
+                    except AssertionError:
+                        print(f"Malformed node representation: {n}")
+                    except KeyError:
+                        print(f"Detected a dangling node: {n['name']}")
+
+        except (KeyError):
+            raise Exception("Malformed input JSON string")
+
+
+class Struct:
+    # https://stackoverflow.com/questions/6866600/how-to-parse-read-a-yaml-file-into-a-python-object
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
 def make_unique_task_id(nodes, datetime_stamp):
     # Create pretty-unique hash value based on input nodes
     # https://www.geeksforgeeks.org/ways-sort-list-dictionaries-values-python-using-lambda-function/
