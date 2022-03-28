d83141979d2de9ebe61c3b0546db10ab8b93fd90
Paul Billing-Ross <pbilling@stanford.edu>
2022-03-28 11:51:11 -0700

    Test custom query

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import pdb
 import json
 import mock
 import neo4j
@@ -47,7 +48,6 @@ class TestQueryRequestWriter(TestCase):
 									   previous_event_id = cls.previous_event_id,
 									   query_name = cls.query_name,
 									   query_parameters = cls.query_parameters)
-
 		message = request.format_json_message()
 		message['header']
 		message['body']
@@ -61,6 +61,83 @@ class TestQueryRequestWriter(TestCase):
 		# Check body values
 		assert message['body']['queryName'] == cls.query_name
 		assert message['body']['queryParameters'] == cls.query_parameters
+
+	@classmethod
+	def test_write_custom_query_request(cls):
+
+		query_name = "mergeFastqNode"
+		query = "MERGE (node:Fastq { uri: $uri, crc32c: $crc32c }) ON CREATE SET node.nodeCreated = timestamp(), node.nodeIteration = 'initial', node.bucket = $bucket, node.componentCount = $componentCount, node.contentType = $contentType, node.crc32c = $crc32c, node.customTime = $customTime, node.etag = $etag, node.eventBasedHold = $eventBasedHold, node.generation = $generation, node.id = $id, node.kind = $kind, node.mediaLink = $mediaLink, node.metadata = $metadata, node.metageneration = $metageneration, node.name = $name, node.selfLink = $selfLink, node.size = $size, node.storageClass = $storageClass, node.temporaryHold = $temporaryHold, node.timeCreated = $timeCreated, node.timeStorageClassUpdated = $timeStorageClassUpdated, node.updated = $updated, node.trellisUuid = $trellisUuid, node.path = $path, node.dirname = $dirname, node.basename = $basename, node.extension = $extension, node.filetype = $filetype, node.gitCommitHash = $gitCommitHash, node.gitVersionTag = $gitVersionTag, node.uri = $uri, node.timeCreatedEpoch = $timeCreatedEpoch, node.timeUpdatedEpoch = $timeUpdatedEpoch, node.timeCreatedIso = $timeCreatedIso, node.timeUpdatedIso = $timeUpdatedIso, node.plate = $plate, node.sample = $sample, node.matePair = $matePair, node.readGroup = $readGroup ON MATCH SET node.nodeIteration = 'merged', node.size = $size, node.timeUpdatedEpoch = $timeUpdatedEpoch, node.timeUpdatedIso = $timeUpdatedIso, node.timeStorageClassUpdated = $timeStorageClassUpdated, node.updated = $updated, node.id = $id, node.crc32c = $crc32c, node.generation = $generation, node.storageClass = $storageClass RETURN node"
+		fastq_query_parameters = {
+			'bucket': 'gcp-bucket-mvp-test-from-personalis',
+			'componentCount': 32,
+			'contentType': 'application/octet-stream',
+			'crc32c': 'ftNG8w==',
+			'customTime': '1970-01-01T00:00:00.000Z',
+			'etag': 'CJmAwYe83OcCEAs=',
+			'eventBasedHold': False,
+			'generation': '1582075915288601',
+			'id': 'gcp-project-test-from-personalis/va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz/1582075915288601',
+			'kind': 'storage#object',
+			'mediaLink': 'https://www.googleapis.com/download/storage/v1/b/gcp-project-test-from-personalis/o/va_mvp_phase2%2FPLATE0%2FSAMPLE0%2FFASTQ%2FSAMPLE0_0_R1.fastq.gz?generation=1582075915288601&alt=media',
+			'metadata': "{'gcf-update-metadata': '2696298877621712'}",
+			'metageneration': '11',
+			'name': 'SAMPLE0_0_R1', 
+			'selfLink': 'https://www.googleapis.com/storage/v1/b/gcp-project-test-from-personalis/o/va_mvp_phase2%2FPLATE0%2FSAMPLE0%2FFASTQ%2FSAMPLE0_0_R1.fastq.gz',
+			'size': 5955984357,
+			'storageClass': 'REGIONAL',
+			'temporaryHold': False,
+			'timeCreated': '2020-02-19T01:31:55.288Z',
+			'timeStorageClassUpdated': '2020-02-19T01:31:55.288Z',
+			'updated': '2022-02-28T21:13:19.739Z', 
+			'trellisUuid': 1234, 
+			'path': 'va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz', 
+			'dirname': 'va_mvp_phase2/PLATE0/SAMPLE0/FASTQ', 
+			'basename': 'SAMPLE0_0_R1.fastq.gz', 
+			'extension': 'fastq.gz', 
+			'filetype': 'gz', 
+			'gitCommitHash': 'abcd5', 
+			'gitVersionTag': None, 
+			'uri': 'gs://gcp-bucket-mvp-test-from-personalis/va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz', 
+			'timeCreatedEpoch': 1582075915.288, 
+			'timeUpdatedEpoch': 1646082799.739, 
+			'timeCreatedIso': '2020-02-19T01:31:55.288000+00:00', 
+			'timeUpdatedIso': '2022-02-28T21:13:19.739000+00:00', 
+			'plate': 'PLATE0', 
+			'sample': 'SAMPLE0', 
+			'matePair': 1, 
+			'readGroup': 0
+		}
+		publish_to = "check-triggers"
+
+		request = trellis.QueryRequestWriter(
+					sender = cls.sender,
+					seed_id = cls.seed_id,
+					previous_event_id = cls.previous_event_id,
+					query_name = query_name,
+					query = query,
+					query_parameters = fastq_query_parameters,
+					custom = True,
+					write_transaction = True,
+					publish_to = "check-triggers",
+					returns = {"node": "node"})
+		message = request.format_json_message()
+		header = message['header']
+		body = message['body']
+
+		#pdb.set_trace()
+
+		# Check header values
+		assert message['header']['messageKind'] == "queryRequest"
+		assert message['header']['sender'] == cls.sender
+		assert message['header']['seedId'] == cls.seed_id
+		assert message['header']['previousEventId'] == cls.previous_event_id
+
+		# Check body values
+		assert body['queryName'] == query_name
+		assert len(body['query']) == 1602
+		assert body['queryParameters'] == fastq_query_parameters
+		assert body['writeTransaction'] == True
+		assert body['publishTo'] == publish_to
 
 
 class TestQueryRequestReader(TestCase):
@@ -77,6 +154,7 @@ class TestQueryRequestReader(TestCase):
 			'body': {
 				'queryName': 'dummyTrigger',
 				'queryParameters': {},
+				'custom': False
             }
         }
 		data_str = json.dumps(data)
@@ -117,7 +195,8 @@ class TestQueryRequestReader(TestCase):
 					"stopTimeEpoch": "1636067813.95614",
 					"stoppedBy": "genomics-api",
 					"status": "STOPPED",
-				}
+				},
+				'custom': False
 			}
 		}
 		data_str = json.dumps(data)
@@ -144,6 +223,92 @@ class TestQueryRequestReader(TestCase):
 			"stoppedBy": "genomics-api",
 			"status": "STOPPED",
 		}
+
+	@classmethod
+	def test_parse_custom_query_request(cls):
+		data = {
+				'header': {
+						   'messageKind': 'queryRequest', 
+						   'sender': 'trellis-create-blob-node', 
+						   'seedId': 123, 
+						   'previousEventId': 345
+				}, 
+				'body': {
+					    'queryName': 'mergeFastqNode', 
+					    'queryParameters': {
+					    	'bucket': 'gcp-bucket-mvp-test-from-personalis', 
+					    	'componentCount': 32, 
+					    	'contentType': 'application/octet-stream', 
+					    	'crc32c': 'ftNG8w==', 
+					    	'customTime': '1970-01-01T00:00:00.000Z', 
+					    	'etag': 'CJmAwYe83OcCEAs=', 
+					    	'eventBasedHold': False, 
+					    	'generation': '1582075915288601', 
+					    	'id': 'gcp-project-test-from-personalis/va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz/1582075915288601', 
+					    	'kind': 'storage#object', 
+					    	'mediaLink': 'https://www.googleapis.com/download/storage/v1/b/gcp-project-test-from-personalis/o/va_mvp_phase2%2FPLATE0%2FSAMPLE0%2FFASTQ%2FSAMPLE0_0_R1.fastq.gz?generation=1582075915288601&alt=media', 
+					    	'metadata': "{'gcf-update-metadata': '2696298877621712'}", 
+					    	'metageneration': '11', 
+					    	'name': 'SAMPLE0_0_R1', 
+					    	'selfLink': 'https://www.googleapis.com/storage/v1/b/gcp-project-test-from-personalis/o/va_mvp_phase2%2FPLATE0%2FSAMPLE0%2FFASTQ%2FSAMPLE0_0_R1.fastq.gz', 
+					    	'size': 5955984357, 
+					    	'storageClass': 'REGIONAL', 
+					    	'temporaryHold': False, 
+					    	'timeCreated': '2020-02-19T01:31:55.288Z', 
+					    	'timeStorageClassUpdated': '2020-02-19T01:31:55.288Z', 
+					    	'updated': '2022-02-28T21:13:19.739Z', 
+					    	'trellisUuid': 1234, 
+					    	'path': 'va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz', 
+					    	'dirname': 'va_mvp_phase2/PLATE0/SAMPLE0/FASTQ', 
+					    	'basename': 'SAMPLE0_0_R1.fastq.gz', 
+					    	'extension': 'fastq.gz', 
+					    	'filetype': 'gz', 
+					    	'gitCommitHash': 'abcd5', 
+					    	'gitVersionTag': None, 
+					    	'uri': 'gs://gcp-bucket-mvp-test-from-personalis/va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz', 
+					    	'timeCreatedEpoch': 1582075915.288, 
+					    	'timeUpdatedEpoch': 1646082799.739, 
+					    	'timeCreatedIso': '2020-02-19T01:31:55.288000+00:00', 
+					    	'timeUpdatedIso': '2022-02-28T21:13:19.739000+00:00', 
+					    	'plate': 'PLATE0', 
+					    	'sample': 'SAMPLE0', 
+					    	'matePair': 1, 
+					    	'readGroup': 0
+					    }, 
+			    		'custom': True, 
+			    		'query': "MERGE (node:Fastq { uri: $uri, crc32c: $crc32c }) ON CREATE SET node.nodeCreated = timestamp(), node.nodeIteration = 'initial', node.bucket = $bucket, node.componentCount = $componentCount, node.contentType = $contentType, node.crc32c = $crc32c, node.customTime = $customTime, node.etag = $etag, node.eventBasedHold = $eventBasedHold, node.generation = $generation, node.id = $id, node.kind = $kind, node.mediaLink = $mediaLink, node.metadata = $metadata, node.metageneration = $metageneration, node.name = $name, node.selfLink = $selfLink, node.size = $size, node.storageClass = $storageClass, node.temporaryHold = $temporaryHold, node.timeCreated = $timeCreated, node.timeStorageClassUpdated = $timeStorageClassUpdated, node.updated = $updated, node.trellisUuid = $trellisUuid, node.path = $path, node.dirname = $dirname, node.basename = $basename, node.extension = $extension, node.filetype = $filetype, node.gitCommitHash = $gitCommitHash, node.gitVersionTag = $gitVersionTag, node.uri = $uri, node.timeCreatedEpoch = $timeCreatedEpoch, node.timeUpdatedEpoch = $timeUpdatedEpoch, node.timeCreatedIso = $timeCreatedIso, node.timeUpdatedIso = $timeUpdatedIso, node.plate = $plate, node.sample = $sample, node.matePair = $matePair, node.readGroup = $readGroup ON MATCH SET node.nodeIteration = 'merged', node.size = $size, node.timeUpdatedEpoch = $timeUpdatedEpoch, node.timeUpdatedIso = $timeUpdatedIso, node.timeStorageClassUpdated = $timeStorageClassUpdated, node.updated = $updated, node.id = $id, node.crc32c = $crc32c, node.generation = $generation, node.storageClass = $storageClass RETURN node", 
+			    		'writeTransaction': True, 
+			    		'publishTo': 'check-triggers', 
+			    		'returns': {'node': 'node'}
+			    }
+		}
+		data_str = json.dumps(data)
+		data_utf8 = data_str.encode('utf-8')
+		event = {'data': base64.b64encode(data_utf8)}
+
+		request = trellis.QueryRequestReader(
+											 mock_context,
+											 event)
+
+		# Check header values
+		assert request.message_kind == "queryRequest"
+		assert request.sender == "trellis-create-blob-node"
+		assert request.seed_id == 123
+		assert request.previous_event_id == 345
+
+		# Check body values
+		assert request.query_name == data['body']['queryName']
+		assert request.query_parameters['bucket'] == 'gcp-bucket-mvp-test-from-personalis'
+		assert request.query_parameters['metadata'] == "{'gcf-update-metadata': '2696298877621712'}"
+		assert request.query_parameters['size'] == 5955984357
+		assert request.query_parameters['temporaryHold'] == False 
+		assert request.query_parameters['trellisUuid'] == 1234 
+		assert request.query_parameters['uri'] == 'gs://gcp-bucket-mvp-test-from-personalis/va_mvp_phase2/PLATE0/SAMPLE0/FASTQ/SAMPLE0_0_R1.fastq.gz'
+
+		assert request.query == data['body']['query']
+		assert request.write_transaction == data['body']['writeTransaction']
+		assert request.publish_to == data['body']['publishTo']
+		assert request.returns == data['body']['returns']
 
 
 class TestQueryResponseWriter(TestCase):
