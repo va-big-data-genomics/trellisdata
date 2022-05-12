91015f6f04e362954dda404b0e6d6336264c2c5e
Paul Billing-Ross <pbilling@stanford.edu>
2022-05-11 17:58:11 -0700

    Add tests for custom queries

--- tests/test_database_query.py
+++ tests/test_database_query.py
@@ -68,7 +68,7 @@ class TestDatabaseQuery(TestCase):
 		custom_query = DatabaseQuery(
             name = name,
             query = query,
-            parameters = {},
+            required_parameters = {},
             write_transaction = True,
             publish_to = publish_to,
             returns = returns)
@@ -80,11 +80,51 @@ class TestDatabaseQuery(TestCase):
 
 	@classmethod
 	def test_load_query_from_file(cls):
-		with open("sample-database-query.yaml", "r") as file_handle:
+		with open("sample_inputs/pilot-db-queries.yaml", "r") as file_handle:
 			queries = yaml.load_all(file_handle, Loader=yaml.FullLoader)
 			# Convert generator to list
 			queries = list(queries)
 
-		assert len(queries) == 1
-		query = queries[0]
-		assert query.name == 'relateTbiToGvcf'
+		assert len(queries) == 8
+
+	@classmethod
+	def test_create_custom_query_with_params(cls):
+		name = "mergeCustomNode"
+		query = "MERGE (n:Custom) SET n.cov = $AlignmentCoverage, n.assay = $AssayType, n.blood = $BloodType RETURN n as custom"
+		publish_to = "trellis-check-triggers"
+		returns = {"custom": "node"}
+		
+		query_parameters = {'AlignmentCoverage': '33.7162', 'AssayType': 'WGS', 'BloodType': 'O'}
+
+		required_parameters = {}
+		for key, value in query_parameters.items():
+			required_parameters[key] = type(value).__name__
+
+		custom_query = DatabaseQuery(
+			name = name,
+			query = query,
+			required_parameters = required_parameters,
+			write_transaction = True,
+			publish_to = publish_to,
+			returns = returns)
+
+		assert custom_query.required_parameters['AlignmentCoverage'] == "str"
+		assert custom_query.required_parameters['AssayType'] == "str"
+		assert custom_query.required_parameters['BloodType'] == "str"
+
+	@classmethod
+	def test_create_custom_query_no_returns(cls):
+		name = "mergeCustomNode"
+		query = "MERGE (n:Custom) RETURN n as custom"
+		publish_to = "trellis-check-triggers"
+
+		custom_query = DatabaseQuery(
+            name = name,
+            query = query,
+            required_parameters = {},
+            write_transaction = True,
+            publish_to = publish_to)
+
+		assert custom_query.name == name
+		assert custom_query.query == query
+		assert custom_query.publish_to == publish_to
